3c312e7b82c9d3df51885b6b83025134ac10ed39
mazurro1 <mazul96.hm@gmail.com>
2019-05-20 14:04:35 +0200

    v1_3

--- src/header.js
+++ src/header.js
@@ -3,14 +3,38 @@ import './sass/header.scss';
 
 class Header extends React.Component {
     state = {
+        welcome: '',
+        indexText: 0,
+    }
 
+    componentDidMount() {
+        const text = 'Witaj na moim portfolio!';
+        this.indexInterval = setInterval(() => {
+            this.setState(prevState => ({
+                indexText: prevState.indexText + 1,
+                welcome: prevState.welcome + text[prevState.indexText],
+            }))
+
+            if (this.state.indexText === text.length) {
+                clearInterval(this.indexInterval);
+            }
+        }, 50)
+    }
+
+    componentWillUnmount() {
+        if (this.state.indexText === 5) {
+            clearInterval(this.indexInterval);
+        }
     }
 
     render() {
         return (
-            <>
-                <h1>header</h1>
-            </>
+            <div className="header">
+                <h1>{this.state.welcome}</h1>
+                <div>
+
+                </div>
+            </div>
         )
     }
 }
