43bd3c88bf81351d1722c28b9d7198b099b0101c
mazurro1 <mazul96.hm@gmail.com>
2019-05-27 14:49:02 +0200

    v1_15 (icons in skills)

--- src/sectionComponents/skills.js
+++ src/sectionComponents/skills.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../sass/skills.scss';
 import arrowUp from '../images/arrowUp.png';
+import html from '../images/icon_html5.png';
 
 
 class Skills extends React.Component {
@@ -9,6 +10,14 @@ class Skills extends React.Component {
         classArrow: 'noActive',
         classArrowRotate: 'noActiveArrow'
     }
+
+    icons = [
+        {
+            id: 1,
+            alt: 'icon html',
+            img: html,
+        },
+    ]
 
     handleArrow = e => {
         if (this.state.arrowActive) {
@@ -32,14 +41,19 @@ class Skills extends React.Component {
 
 
         return (
-            <div id="portfolio" className={this.state.classArrow}>
+            <div id="skills" className={this.state.classArrow}>
                 <div className="container">
                     <div className="arrow" onClick={this.handleArrow}>
                         <img src={arrowUp} alt="arrow" className={this.state.classArrowRotate} />
                     </div>
-                    <h1>Skills</h1>
+                    <h1>Umiejętności</h1>
+                    <div className="technology">
+                        <p>Lista technologii, ktore opanowalem na poziomie podstawowym i aktualnie je rozwijam.</p>
+                    </div>
                     <div className="row">
-
+                        <div className="col">
+                            <CreateIcons icons={this.icons} />
+                        </div>
                     </div>
                 </div>
             </div>
@@ -47,7 +61,14 @@ class Skills extends React.Component {
     }
 }
 
-
+const CreateIcons = ({ icons }) => {
+    const create = icons.map(icon => { })
+    return (
+        <>
+            <img src="" alt="" />
+        </>
+    )
+}
 
 
 export default Skills;
