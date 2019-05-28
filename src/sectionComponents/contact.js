80872ccf7ad57ef2e0aee13cee5a82ffdb2f80a0
mazurro1 <mazul96.hm@gmail.com>
2019-05-28 11:43:00 +0200

    v1_18 (skills update, contact finished)

--- src/sectionComponents/contact.js
+++ src/sectionComponents/contact.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import '../sass/contact.scss';
 import arrowUp from '../images/arrowUp.png';
+import facebook from '../images/facebook.png';
+import messenger from '../images/messenger.png';
+import linkedin from '../images/linkedin.png';
+
 
 
 class Contact extends React.Component {
@@ -9,6 +13,30 @@ class Contact extends React.Component {
         classArrow: 'noActive',
         classArrowRotate: 'noActiveArrow'
     }
+
+    contactList = [
+        {
+            id: 1,
+            target: "_blank",
+            image: facebook,
+            link: 'https://www.facebook.com/Hubaa01',
+            alt: 'Facebook',
+        },
+        {
+            id: 2,
+            target: "_blank",
+            image: messenger,
+            link: 'https://web.facebook.com/messages/t/Hubaa01',
+            alt: 'Messenger',
+        },
+        {
+            id: 3,
+            target: "_blank",
+            image: linkedin,
+            link: 'https://www.linkedin.com/in/hubert-mazur-378a19113/',
+            alt: 'Linkedin',
+        },
+    ];
 
     handleArrow = e => {
         if (this.state.arrowActive) {
@@ -32,15 +60,15 @@ class Contact extends React.Component {
 
 
         return (
-            <div id="aboutMe" className={this.state.classArrow}>
+            <div id="contact" className={this.state.classArrow}>
                 <div className="container">
                     <div className="arrow" onClick={this.handleArrow}>
                         <img src={arrowUp} alt="arrow" className={this.state.classArrowRotate} />
                     </div>
                     <h1>Kontakt</h1>
                     <p>Masz pytanie? Napisz!</p>
-                    <div className="row">
-
+                    <div className="row contactIcon">
+                        <CreateIconContact contactList={this.contactList} />
                     </div>
                 </div>
             </div>
@@ -48,6 +76,23 @@ class Contact extends React.Component {
     }
 }
 
+
+const CreateIconContact = ({ contactList }) => {
+    const doIcons = contactList.map(item => {
+        return (
+            <div className="col" key={item.id}>
+                <a href={item.link} target={item.target}>
+                    <img src={item.image} alt={item.alt} />
+                </a>
+            </div>
+        )
+    })
+    return (
+        <>
+            {doIcons}
+        </>
+    )
+}
 
 
 
